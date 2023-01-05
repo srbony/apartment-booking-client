e934938a6f5dfaab77d66fff5c58f302dd9fc09a
srbony <srabonycse@gmail.com>
2023-01-05 09:58:24 +0600

    added authentication

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  // COnfig code goes here
+  apiKey: "AIzaSyCxlhq_2q59612bdoot_SUhkn7l9FKyVjI",
+  authDomain: "aircnc-a7e6f.firebaseapp.com",
+  projectId: "aircnc-a7e6f",
+  storageBucket: "aircnc-a7e6f.appspot.com",
+  messagingSenderId: "241910773991",
+  appId: "1:241910773991:web:452b932355f1a72badca42"
+}
+
+const app = initializeApp(firebaseConfig)
+
+export default app
